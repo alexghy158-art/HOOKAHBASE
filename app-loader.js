d10eb0de04448b78dc74c2688b3f73d0243998fc
alexghy158-art <alexghy158@gmail.com>
2026-09-25 00:43:25 +0400

    Restore site via app-loader + fixed chunks; budget tab

--- app-loader.js
+++ app-loader.js
@@ -1,11 +1,9 @@
 (async function () {
-  var parts = await Promise.all([0, 1].map(function (i) {
-    return fetch("app" + i + ".b64").then(function (r) { return r.ok ? r.text() : ""; });
+  var parts = await Promise.all([0,1,2,3].map(function (i) {
+    return fetch("aj" + i + ".b64").then(function (r) { return r.ok ? r.text() : ""; });
   }));
-  var b64 = parts.join("").replace(/\n/g, "");
-  var code = atob(b64);
   var s = document.createElement("script");
-  s.textContent = code;
+  s.textContent = atob(parts.join(""));
   document.head.appendChild(s);
   var b = document.createElement("script");
   b.src = "budget.js";
